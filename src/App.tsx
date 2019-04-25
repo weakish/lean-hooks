189b06bdca0901ea68ebbe7775757ee90581e68a
Jang Rush <weakish@gmail.com>
2019-04-25 18:41:09 +0800

    :new: register with username and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {useForm} from 'react-hooks-helper'
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import AV from 'leancloud-storage'
+import './App.css'
 
 interface User {
   [index: string]: string;
@@ -10,24 +11,59 @@ interface User {
 }
 
 const App: React.FC = () => {
+  const [token, setToken] = useState("")
   const [{username, password, email}, setForm] = useForm<User>({
     username: '',
     password: '',
     email: '',
   })
+  const [submitted, setSubmitted] = useState(false)
+
+  useEffect(() => {
+    AV.init({
+      appId: "3AJTBW5QyGw17hLxNN3njGHW-gzGzoHsz",
+      appKey: "P8cKdEvRxusfuR27MuhvFOzr",
+    })
+  }, [])
+  useEffect(() => {
+    const fetchToken = async () => {
+      if (submitted && token === "") {
+        const user = new AV.User()
+        user.setUsername(username)
+        user.setPassword(password)
+        user.setEmail(email)
+        const authenticatedUser = await user.signUp()
+        const sessionToken = authenticatedUser.getSessionToken()
+        setToken(sessionToken)
+      }
+    }
+    fetchToken()
+  }, [submitted, username, password, email, token])
   
   const userProperties = ["username", "password", "email"]
   const inputFields = userProperties.map((p, i) =>
-      <div className="signup-form">
-        <label htmlFor={p}>{p}: </label>
+      <div key={p} className="register-form">
+        <label htmlFor={p}>{p}</label>
         <input type={p === "username" ? "text" : p}
-                value={p === "username" ? username : (p === "password" ? password : email)} id={p} name={p} required onChange={setForm} />
+                value={p === "username" ? username : (p === "password" ? password : email)} id={p} name={p} onChange={setForm} />
       </div>
   )
-  
+  const note = <div className="register-form-note">
+    <p>All input fields are optional:</p>
+    <ul>
+      <li>If email is unspecified, we will left it as empty. Thus you cannot receive notifications and recover your account via email.</li>
+      <li>If username is unspecified but email is specified, we will use email as username.</li>
+      <li>If both username and email are unspecified, we will auto generate an username for you.</li>
+      <li>If password is unspecified, we will auto generate a random onetime password.</li>
+    </ul>
+  </div>
   return (
     <div className="App">
-      {inputFields}
+        {inputFields}
+        <div className="register-form">
+          <input type="submit" value="register" onClick={() => setSubmitted(true)} />
+        </div>
+      <p>token: {token}</p>
     </div>
   );
 }
